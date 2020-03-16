2cc22b52aa87b4820c0000d26738caabd3e8642a
leesomi <122369@naver.com>
2020-03-16 20:48:57 +0900

    token여부에 따라 reload시, 사용자정보 조회

--- src/App.js
+++ src/App.js
@@ -9,12 +9,45 @@ import MyPage from "./components/MyPage/MyPage";
 import UserController from "./components/UserController";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import gStore from "./store";
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       keyword: ""
     };
+  }
+
+  //사용자 정보 조회 API
+  initializeUserInfo() {
+    console.log(
+      "store init jwt-token : ",
+      window.localStorage.getItem("jwt-token")
+    );
+
+    var token = window.localStorage.getItem("jwt-token");
+    //token이 았으면 사용자 정보 조회 없다면 조회하지 않음
+    if (token != null && token.length > 0) {
+      var oOption = {
+        headers: {
+          "jwt-token": token
+        }
+      };
+      fetch(
+        process.env.REACT_APP_BACKEND_API_URL + "/user/jwt-auth/tokeninfo",
+        oOption
+      )
+        .then(res => res.json())
+        .then(data => {
+          gStore.dispatch({ type: "LOGIN", jwt_token: "Y" });
+        })
+        .catch(err => alert(err));
+    }
+  }
+  componentDidMount() {
+    //사용자 정보 조회
+    this.initializeUserInfo();
   }
   render() {
     console.log("App render");
